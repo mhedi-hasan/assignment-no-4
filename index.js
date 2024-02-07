791a68e552858307e42029c4e06ca42a49857dbb
Mehedi Hasan <mhedih32@gmail.com>
2024-02-07 21:26:46 +0600

    2nd problem solved

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-// 1st problem solved
+//----> 1st problem solved <----
 function calculateMoney(ticketSale) {
-    if (ticketSale <= 0) {
+  if (ticketSale <= 0) {
     return "Please give positive number";
-    }
-    const totalEarn = ticketSale * 120 - (500 + 8 * 50);
-    return totalEarn;
+  }
+  const totalEarn = ticketSale * 120 - (500 + 8 * 50);
+  return totalEarn;
 }
-// console.log(calculateMoney(10));
+// console.log(calculateMoney(-10));
+
+// ------------------------------------------------
+
+//----> 2nd problem solved <----
+function checkName(name) {
+  if (typeof name !== "string") {
+    return "Please give string value";
+  }
+  const lastCharacters = name.slice(-1);
+  const validCharacters = ['A', 'y', 'i' , 'e' , 'o' , 'u', 'w'];
+
+  if (validCharacters.includes(lastCharacters)) {
+    return "Good Name";
+  } else {
+    return "Bad Name";
+  }
+}
+// console.log(checkName("Roiden"));
